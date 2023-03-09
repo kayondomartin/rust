94b9967e4cdc07285bbd5e1c446e62f97b7b0c84
kayondo martin <kayondomartin45@gmail.com>
2023-03-09 14:50:56 +0900

    modified .gitignore

---
+++ src/test/rustdoc-gui/src/staged_api/target/debug/deps/staged_api-c75f57c73f6d235f.d
@@ -0,0 +1,5 @@
+/home/martin/projects/meta-rust/rust/src/test/rustdoc-gui/src/staged_api/target/debug/deps/staged_api-c75f57c73f6d235f.rmeta: lib.rs
+
+/home/martin/projects/meta-rust/rust/src/test/rustdoc-gui/src/staged_api/target/debug/deps/staged_api-c75f57c73f6d235f.d: lib.rs
+
+lib.rs:
---
+++ src/test/rustdoc-gui/src/test_docs/target/debug/build/test_docs-a037da6013a9dc59/build_script_build-a037da6013a9dc59.d
@@ -0,0 +1,5 @@
+/home/martin/projects/meta-rust/rust/src/test/rustdoc-gui/src/test_docs/target/debug/build/test_docs-a037da6013a9dc59/build_script_build-a037da6013a9dc59: build.rs
+
+/home/martin/projects/meta-rust/rust/src/test/rustdoc-gui/src/test_docs/target/debug/build/test_docs-a037da6013a9dc59/build_script_build-a037da6013a9dc59.d: build.rs
+
+build.rs:
