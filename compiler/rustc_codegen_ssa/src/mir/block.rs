757663a9e82d94268a42fa22538a7f25463cb7d3
kayondomartin <kayondomartin45@gmail.com>
2023-06-02 20:03:01 +0900

    inlined assembly for MPK

--- compiler/rustc_codegen_ssa/src/mir/block.rs
+++ compiler/rustc_codegen_ssa/src/mir/block.rs
@@ -797,7 +797,7 @@ impl<'a, 'tcx, Bx: BuilderMethods<'a, 'tcx>> FunctionCx<'a, 'tcx, Bx> {
                                 let inner_ty = self.monomorphize(type_);
                                 let type_id = if bx.tcx().is_special_ty(inner_ty) ||
                                 (bx.tcx().sess.opts.unstable_opts.meta_update_struct_kind.unwrap().eq(&rustc_session::config::MetaUpdateStructKind::Implicit) &&
-                                bx.tcx().contains_special_ty(inner_ty)){ 1 }else{bx.tcx().type_id_hash(inner_ty)};
+                                bx.tcx().contains_special_ty(inner_ty)){ 1 }else{0};
                                 self.smart_pointer_inner_ty = Some(type_id);
                                 break;
                             }
